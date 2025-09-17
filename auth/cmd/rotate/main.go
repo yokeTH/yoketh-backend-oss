08c3b558ebbe220597b534c8b1a14528432727e1
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-09-17 23:51:32 +0700

    feat(auth): rotate command

---
+++ auth/cmd/rotate/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gofiber/fiber/v3/log"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/yokeTH/yoketh-backend-oss/auth/internal/config"
+	"github.com/yokeTH/yoketh-backend-oss/auth/internal/db"
+	"github.com/yokeTH/yoketh-backend-oss/auth/internal/keys"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Warnf("Unable to load .env file: %s", err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	cfg := config.NewFromEnv()
+	dbCfg := cfg.DBConfig
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", dbCfg.Host, dbCfg.Port, dbCfg.User, dbCfg.Password, dbCfg.DBName, dbCfg.SSLMode)
+	sqlDB, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	queries := db.New(sqlDB)
+
+	keyWrapper, err := keys.NewLocalWrapperFromEnv()
+	if err != nil {
+		panic(err)
+	}
+	keyManager := keys.NewManager(sqlDB, queries, keyWrapper, "auth-service")
+	if err := keyManager.Init(ctx); err != nil {
+		panic(err)
+	}
+
+	keyManager.Rotate(ctx)
+
+	stop()
+}
